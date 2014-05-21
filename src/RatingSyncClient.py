a6de2b1214b36421c67e11bec06f1726cbb7e690
Valentin Trifonov <valentin.trifonov@hotmail.de>
2014-05-21 21:25:24 +0200

    Extended the skeleton and argument parsing

--- src/RatingSyncClient.py
+++ src/RatingSyncClient.py
@@ -14,48 +14,72 @@ Author: Valentin Trifonov (and soon maybe some more people aswell).
 import sys
 
 # variables n shit
-# maybe add some default values?
-music_dir = None
+# TODO add default values
+music_dir = None # maybe add cureent directory as default value?
 server_ip = None
-sync_interval = None
+sync_interval_minutes = 60 # Sync once per hour (default value)
+config_file = "RatingSync.conf" # Default config file
+
+print_help = False
+error = False # Error flag
+error_message = None
 
 def print_info():
-    # TODO config datei einlesen mit --import argument
-    # Set
+    """print info about the usage and arguments of this program and, if specified, an error message."""
     print("Usage: [--path <path>] [--server <server-ip>] [--time <sync time interval>]")
-    print("If no options were specified, RatingSync will try to load the default config file RatingSync.conf")
+    print("If no options were specified, RatingSync will try to load the default config file RatingSync.conf\n")
     # TODO implement that
-    # TODO print some more info about the arguments
+    # TODO continuously extend info to be printed about the arguments
     
+def read_config(path):
+    """Loads settings from the file specified in path."""
+    pass
+    # TODO implement
+
 # parse arguments
 for i in range(1, len(sys.argv)):
     #print(sys.argv[i])
     if sys.argv[i] == "--help":
-        print_info()
+        print_help = True
+        error = False
         break
     elif sys.argv[i] == "--config":
-        print("(load config file and connect)")
-        # TODO implement
-        #read_config(sys.argv[i+1])
-        # skip the next argument as we already processed it
-        # i += 1
+        print("Loading config file...")
+        try:
+            config_file = sys.argv[i+1]
+            i += 1 # skip the next argument as we already processed it
+        except:
+            error = True
+            error_message = "No config file specified."
+        break
     elif sys.argv[i] ==  "--server":
-        print("(set default server and connect)")
+        print("(set default server)")
         try:
             server_ip = sys.argv[i+1]
-            # skip the next argument as we processed it already
-            i += 1
+            i += 1 # skip the next argument as we processed it already
         except: 
             # for instance if there is no argument at index i+1 
             # TODO handle stupid arguments not matching an ip address. use regex to do that.
-            print_info()
-        # TODO implemenet
+            error = True
+            error_message = "No server IP specified."
     elif sys.argv[i] == "--time":
-        print("(set default file monitoring and sync time interval, and connect)")
-        sync_interval = sys.argv[i+1]
-        i += 1
-        # TODO implement
-#else: # if there was no break, start the client and sync
+        try:
+            sync_interval_minutes = sys.argv[i+1]
+            i += 1 # skip the next argument as we processed it already
+        except:
+            error = True
+            error_message = "No sync time interval specified."
+
+#        
+if error:
+    print(error_message)
+    print("Exiting...")
+elif print_help:
+    print_info()
+else:
+    pass
+    # hier kommt die implementation rein
+    # zuerst aber muss ich noch ein bisschen rumprobieren. ich versuche erstmal die dateien einzulesen und ein dict/einen baum daraus aufzubauen und auszugeben
+
     # TODO implement
-    
-#def read_config(path):
+#def build_structure():
