c4e87fc806dbd39b959e0c11989d9a0bad2dc233
Valentin Trifonov <valentin.trifonov@hotmail.de>
2014-05-23 00:23:09 +0200

    using argparse now

--- src/RatingSyncClient.py
+++ src/RatingSyncClient.py
@@ -13,103 +13,65 @@ Author: Valentin Trifonov (and soon maybe some more people aswell).
 # imports n shit
 import sys
 import os
-
-# variables n shit
-# TODO add default values
-music_dir = None # maybe add cureent directory as default value?
-server_ip = None
-sync_interval_minutes = 60 # Sync once per hour (default value)
-config_file = "RatingSync.conf" # Default config file
-
-print_help = False
-error = False # Error flag
-error_message = None
-verbose = False # Verbose flag
-
-def print_info():
-    """print info about the usage and arguments of this program and, if specified, an error message."""
-    print("Usage: [--config <path-to-config-file>] [--dir <path-to-music-directory>] [--server <server-ip>] [--time <sync time interval>]")
-    print("If no options were specified, RatingSync will try to load the default config file RatingSync.conf\n")
-    # TODO implement that
-    # TODO continuously extend info to be printed about the arguments
-    
-def read_config(path):
-    """Loads settings from the file specified in path."""
-    pass
-    # TODO implement
+import re
+import argparse
 
 # parse arguments
-for i in range(1, len(sys.argv)):
-    #print(sys.argv[i])
-    if sys.argv[i] == "--help":
-        print_help = True
-        error = False
-        break
-    elif sys.argv[i] == "--config":
-        print("Loading config file...")
-        try:
-            config_file = sys.argv[i+1]
-            i += 1 # skip the next argument as we already processed it
-        except:
-            error = True
-            error_message = "Invalid config file."
-        break
-    elif sys.argv[i] == "--path":
-        print("Loading music directory...")
-        try:
-            music_dir = sys.argv[i+1] # skip the next argument as we already processed it
-            os.chdir(music_dir)
-        except:
-            error = True
-            error_message = "Invalid music directory."
-    elif sys.argv[i] == "--server":
-        print("(set default server)")
-        try:
-            server_ip = sys.argv[i+1]
-            i += 1 # skip the next argument as we processed it already
-        except: 
-            # for instance if there is no argument at index i+1 
-            # TODO handle stupid arguments not matching an ip address. use regex to do that.
-            error = True
-            error_message = "Invalid server IP."
-    elif sys.argv[i] == "--time":
-        try:
-            sync_interval_minutes = sys.argv[i+1]
-            i += 1 # skip the next argument as we processed it already
-        except:
-            error = True
-            error_message = "No sync time interval specified."
-    elif sys.argv[i] == "-v":
-        verbose = True
+parser = argparse.ArgumentParser(description="Start the Rating syncing client.")
+parser.add_argument("-v", "--verbose", help="verbosity (print more about what is done)", action="store_true")
+subparser = parser.add_subparsers()
+# init per config file
+conf_parser = subparser.add_parser("config", help="load settings per config file")
+conf_parser.add_argument("--file", help="the path to the config file (Default: RatingSync.conf)", default="RatingSync.conf")
+# init per arguments
+arg_parser = subparser.add_parser("opt", help="initialize settings per options")
+arg_parser.add_argument("--path", help="the path to the directory of your music collection", type=str, default=os.curdir)
+arg_parser.add_argument("server", help="the server to connect to")
+arg_parser.add_argument("--time", help="the time interval for syncing the database", type=int, default=60)
+# parse args
+args = parser.parse_args()
         
-# implementation
-if error:
-    print(error_message)
-    print("Exiting...")
-elif print_help:
-    print_info()
+# init
+verbose = args.verbose
+if "file" in args:
+    print("parsing %s (not implemented yet)" % args["file"])
+    # TODO
+    path = None
+    server = None
+    time = None
+elif "server" in args:
+    path = args.path
+    server = args.server
+    time = args.time
 else:
-    music_files = []
-    # Perform a DFS and put all the ( TODO (regex) music) files in the list music_files
-    dfs_stack = [os.curdir]
-    while len(dfs_stack) > 0:
-        item = dfs_stack.pop()
-        if os.path.isdir(item):
-            if verbose:
-                print("adding directory %s to searching list" % item)
-            # push all child files to the stack
-            dfs_stack.extend([item + os.sep + c for c in os.listdir(path=item)])
-        elif os.path.isfile(item):
-            if verbose:
-                print("adding file %s to music list (will filter soon)" % item)
-            # TODO filter only the mp3 files!
-            music_files.append(item)
-        else:
-            print("error, %s is neither a file nor a directory. exiting." % item)
-            break
-    print("dfs stack len", len(dfs_stack))
-    print("music files len", len(music_files))
-    # first I will try a naive implementation using a dict, once that works I will optimize it and find a good data structure
+    parser.print_help()
+    
 
-    # TODO implement
+# implementation
+os.chdir(path)
+music_files = []
+# Perform a DFS and put all the mp3 files in the list music_files
+dfs_stack = [os.curdir]
+while dfs_stack: # while not empty
+    item = dfs_stack.pop()
+    if os.path.isdir(item):
+        if verbose:
+            print("adding dir to searching list: %s" % item)
+        # push all child files to the stack
+        dfs_stack.extend([item + os.sep + c for c in os.listdir(path=item)])
+    elif os.path.isfile(item):
+        if re.search(r".*\.mp3", item):
+            if verbose:
+                print("adding to music list: %s" % item)
+            music_files.append(item)
+        elif verbose:
+            print("not an mp3 file: %s" % item)
+    else:
+        print("error, %s is neither a file nor a directory. exiting." % item)
+        break
+print("music files len", len(music_files))
+
+# first I will try a naive implementation using a dict, once that works I will optimize it and find a good data structure
+# TODO implement
+    
 #def build_structure():
