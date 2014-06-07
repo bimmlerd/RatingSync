646c76921201e79783b9aa0a95db00bf93896fcf
Posedge <Posedge@users.noreply.github.com>
2014-06-07 23:47:08 +0200

    added loading/saving configuration and changed argument structure completely

--- src/RatingSyncClient.py
+++ src/RatingSyncClient.py
@@ -17,6 +17,50 @@ import re
 import argparse
 import time as timemodule
 from mutagen.id3 import ID3
+import json
+
+#preferences
+class prefs:
+    
+    """Preferences for the program. Will try to open RatingSync.conf and load the config on initialisation."""
+    def __init__(self):
+        try:
+            config_file = open("RatingSync.conf", "r")
+            self.prefs = json.load(config_file)
+        except:
+            # well.. there is no file so we shall save it later.
+            pass
+    
+    # all the preferences:
+    prefs = {"server": None, "path": None, "time": None, "sync_intervall": None}
+    
+    def save(self):
+        """write configuration to the default file "RatingSync.conf" """
+        try:
+            config_file = open("RatingSync.conf", "w")
+            json.dump(self.prefs, config_file)
+        except:
+            print "an error occured when trying to write to RatingSync.conf. exiting."
+            sys.exit(2)
+        
+    def setup(self):
+        """ask user for the unset preferences and store them in a file"""
+        while self.prefs["server"] == None:
+            self.prefs['server'] = input("Enter server ip: ")
+            # TODO check ip format!!!
+        while self.prefs["path"] == None:
+            self.prefs["path"] = input("Enter path of music library: ")
+        while self.prefs["time"] == None:
+            self.prefs["time"] = input("Enter sync interval in seconds (min. 60): ")
+            if not isinstance(self.prefs["time"], int) or self.prefs["time"] < 60: # figure out a good default value
+                self.prefs["time"] = None
+        
+    def clear(self):
+        """clear all preferences"""
+        self.prefs['server'] = None
+        self.prefs["path"] = None
+        self.prefs["time"] = None
+        self.prefs["sync_intervall"] = None
 
 # reading tags
 def read_tag(item):
@@ -32,42 +76,64 @@ def read_tag(item):
         else:               return 0
     except:
         return 0
-
+    
 # parse arguments
-parser = argparse.ArgumentParser(description="Start the Rating syncing client.")
-parser.add_argument("-v", "--verbose", help="verbosity (print more about what is done)", action="store_true")
+# top-level parser
+parser = argparse.ArgumentParser(description="Start the Rating syncing client")
+parser.add_argument("-v", "--verbose", action="store_true", help="verbosity (print more about what is done)")
 subparser = parser.add_subparsers()
-# init using config file
-conf_parser = subparser.add_parser("config", help="load settings per config file")
-conf_parser.add_argument("--file", help="the path to the config file (Default: RatingSync.conf)", default="RatingSync.conf")
-# init using arguments
-arg_parser = subparser.add_parser("opt", help="initialize settings per options")
-arg_parser.add_argument("--path", help="the path to the directory of your music collection", type=str, default=os.curdir)
-arg_parser.add_argument("server", help="the server to connect to")
-arg_parser.add_argument("--time", help="the time interval for syncing the database", type=int, default=60)
+# configuration
+conf_parser = subparser.add_parser("config", help="set preferences")
+conf_parser.set_defaults(which="config")
+conf_parser.add_argument("--setup", action="store_true", help="Set all parameters")
+conf_parser.add_argument("--path", help="the path to the directory of your music collection.", type=str)
+conf_parser.add_argument("--server", help="the server to connect to.")
+conf_parser.add_argument("--time", help="the time interval in seconds for syncing the database.", type=int)
+#arg_parser.add_argument("--ratings-format", help="the format for ratings to be saved in files.", type=str)
+#arg_parser.add_argument("--create-playlists", help="automatically create or update playlists for every star rating")
+# run
+run_parser = subparser.add_parser("run", help="start syncing client")
+run_parser.set_defaults(which="run")
+# sync now
+sync_parser = subparser.add_parser("sync", help="sync database now and exit")
+sync_parser.set_defaults(which="sync")
+# run in background
+daemon_parser = subparser.add_parser("daemon", help="run in background") # TODO
+daemon_parser.set_defaults(which="daemon")
 # parse args
 args = parser.parse_args()
-        
+
 # init
 verbose = args.verbose
-if "file" in args:
-    print("parsing %s (not implemented yet)" % args["file"])
-    # TODO
-    path = None
-    server = None
-    time = None
-elif "server" in args:
-    path = args.path
-    server = args.server
-    time = args.time
+config = prefs()
+if args.which == "config":
+    if args.setup:
+        config.clear()
+    if not args.path == None:
+        config.prefs["path"] = os.path.realpath(args.path)
+    if not args.server == None:
+        config.prefs["server"] = args.server
+    if not args.time == None:
+        config.prefs["time"] = args.time
+    if args.setup:
+        config.setup()
+    config.save()
+    sys.exit(0)
+elif args.which == "run":
+    config.setup() # make sure everything is set
+    # TODO implement
+elif args.which == "sync":
+    config.setup()
+elif args.which == "daemon":
+    config.setup()
+    # TODO implement
 else:
-    parser.print_help()
     sys.exit(1)
 
 # implementation    
 starttime = timemodule.time() # performance measuring
 
-os.chdir(path)
+os.chdir(config.prefs["path"])
 music_files = {}
 # Perform a DFS and put all the mp3 files in the list music_files
 dfs_stack = [os.path.realpath(os.curdir)]
