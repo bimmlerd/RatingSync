fac068efc1af945294bf3821990c875a710c022c
Posedge <Posedge@users.noreply.github.com>
2014-06-10 23:45:25 +0200

    had to restructure the main file. wanted to push this to master first.

--- src/RatingSyncClient.py
+++ src/RatingSyncClient.py
@@ -18,6 +18,7 @@ import Ratings
 import Song
 import json
 import LocalDatabase
+import time as timemodule
 
 #preferences
 class prefs:
@@ -66,6 +67,7 @@ class prefs:
         """clear all preferences"""
         self.prefs = None
 
+"""
 # reading tags
 def read_tag(item):
     # maybe throw exeption if file is not an mp3 file?
@@ -76,70 +78,105 @@ def read_tag(item):
         return Ratings.StarsFromByte(rating, Ratings.RatingProvider.WinAmp)
     except:
         return 0
-    
-# parse arguments
-# top-level parser
-parser = argparse.ArgumentParser(description="Start the Rating syncing client")
-parser.add_argument("-v", "--verbose", action="store_true", help="verbosity (print more about what is done)")
-subparser = parser.add_subparsers()
-# configuration
-conf_parser = subparser.add_parser("config", help="set preferences")
-conf_parser.set_defaults(which="config")
-conf_parser.add_argument("--setup", action="store_true", help="Set all parameters")
-conf_parser.add_argument("--path", help="the path to the directory of your music collection.", type=str)
-conf_parser.add_argument("--server", help="the server to connect to.")
-conf_parser.add_argument("--time", help="the time interval in seconds for syncing the database.", type=int)
-#arg_parser.add_argument("--ratings-format", help="the format for ratings to be saved in files.", type=str)
-#arg_parser.add_argument("--create-playlists", help="automatically create or update playlists for every star rating")
-# run
-run_parser = subparser.add_parser("run", help="start syncing client")
-run_parser.set_defaults(which="run")
-# sync now
-sync_parser = subparser.add_parser("sync", help="sync database now and exit")
-sync_parser.set_defaults(which="sync")
-# run in background
-daemon_parser = subparser.add_parser("daemon", help="run in background") # TODO
-daemon_parser.set_defaults(which="daemon")
-# parse args
-args = parser.parse_args()
+"""
 
-# init
-verbose = args.verbose
-config = prefs()
-if args.which == "config":
-    # TODO Check the input of the commands!!!
-    if args.setup:
-        config.clear()
-    if not args.path == None:
-        config.prefs["path"] = os.path.abspath(args.path)
-    if not args.server == None:
-        config.prefs["server"] = args.server
-    if not args.time == None:
-        config.prefs["time"] = args.time
-    if args.setup:
+def parse_args():
+    """Initialize the program. Parse arguments and set preferences."""
+    # parse arguments
+    # top-level parser
+    parser = argparse.ArgumentParser(description="Start the Rating syncing client")
+    parser.add_argument("-v", "--verbose", action="store_true", help="verbosity (print more about what is done)")
+    subparser = parser.add_subparsers()
+    # configuration
+    conf_parser = subparser.add_parser("config", help="set preferences")
+    conf_parser.set_defaults(which="config")
+    conf_parser.add_argument("--setup", action="store_true", help="Set all parameters")
+    conf_parser.add_argument("--path", help="the path to the directory of your music collection.", type=str)
+    conf_parser.add_argument("--server", help="the server to connect to.")
+    conf_parser.add_argument("--time", help="the time interval in seconds for syncing the database.", type=int)
+    #arg_parser.add_argument("--ratings-format", help="the format for ratings to be saved in files.", type=str)
+    #arg_parser.add_argument("--create-playlists", help="automatically create or update playlists for every star rating")
+    # run
+    run_parser = subparser.add_parser("run", help="start syncing client")
+    run_parser.set_defaults(which="run")
+    # sync now
+    sync_parser = subparser.add_parser("sync", help="sync database now and exit")
+    sync_parser.set_defaults(which="sync")
+    # run in background
+    daemon_parser = subparser.add_parser("daemon", help="run in background") # TODO
+    daemon_parser.set_defaults(which="daemon")
+    # stop the process in background
+    stop_parser = subparser.add_parser("stop", help="stop the process running in background")
+    stop_parser.set_defaults(which="stop")
+    # parse args
+    return parser.parse_args()
+
+def init(args):
+    config = prefs()
+    if args.which == "config":
+        # TODO Check the input of the commands!!!
+        if args.setup:
+            config.clear()
+        if not args.path == None:
+            config.prefs["path"] = os.path.abspath(args.path)
+        if not args.server == None:
+            config.prefs["server"] = args.server
+        if not args.time == None:
+            config.prefs["time"] = args.time
+        if args.setup:
+            config.setup()
+        config.save()
+        sys.exit(0)
+    elif args.which == "run":
+        config.setup() # make sure everything is set
+    elif args.which == "sync":
         config.setup()
-    config.save()
-    sys.exit(0)
-elif args.which == "run":
-    config.setup() # make sure everything is set
-    # TODO implement
-elif args.which == "sync":
-    config.setup()
-elif args.which == "daemon":
-    config.setup()
-    # TODO implement
-else:
-    sys.exit(1)
+    elif args.which == "daemon":
+        config.setup()
+        # TODO implement
+    elif args.which == "stop":
+        pass # TODO stop the daemon
+    else:
+        sys.exit(1)
+    return config
 
 # TODO running in background
 
-# check path
-path = config.prefs["path"]
-if not os.path.exists(path):
-    print "Invalid path: {0}".format(path)
-    config.prefs["path"] = None
-    config.setup()
+def start(args, config):
+    """start syncing"""
+    # check path
+    path = config.prefs["path"]
+    if not os.path.exists(path):
+        print "Invalid path: {0}".format(path)
+        config.prefs["path"] = None
+        config.setup()
+    
+    while True:
+        # TODO check server avialability first!
+        print "Scanning music library..."
+        LocalDatabase.collect(config, args.verbose)
+        print "Syncing with server..."
+        LocalDatabase.upload()
+        # TODO await response, commit changes
+        
+        if args.which == "sync":
+            break
+        
+        if args.verbose:
+            print "Next synchronization in {0} minutes. Press ctrl+c to exit.".format(config.prefs["time"])
+        timemodule.sleep(config.prefs['time']*60)
 
-LocalDatabase.collect(config, verbose)
-LocalDatabase.upload()
-# TODO wait, then repeat
+def run():
+    """run the program according to the preferences/arguments"""
+    args = parse_args()
+    config = init(args)
+    if not args.which == "daemon":
+        start(args, config)
+    else:
+        pass # TODO
+
+if __name__ == "__main__":
+    try:
+        run()
+    except KeyboardInterrupt:
+        print "Exiting..."
